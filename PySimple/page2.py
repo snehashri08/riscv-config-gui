9243321372f3d92dd6c39a8421e34cfa63bf9dff
sneha <snehashrisivaraman@gmail.com>
2021-08-20 15:48:32 +0530

    adding page 2 with index and separate csrs

---
+++ PySimple/page2.py
@@ -0,0 +1,121 @@
+import os
+import logging
+import shutil
+import PySimpleGUI as sg
+import riscv_config.checker as riscv_config
+import riscv_config.utils as utils
+import ruamel
+from ruamel.yaml import YAML
+import yaml as pyyaml
+import re
+yaml = YAML(typ="rt")
+yaml.default_flow_style = False
+yaml.allow_unicode = True
+global ispec, wdir, isa_yaml, rvxlen
+global csr_name, values, sub_field, window
+from collections import OrderedDict
+def update_fields():
+   '''
+    Function to update all changes done by the user'''
+   for  k in list(set(isa_yaml['hart0'][csr_name].keys())-set(['rv32', 'rv64'])):
+      if k in {'address', 'reset-val'}:
+         isa_yaml['hart0'][csr_name][k]=int(values['-'+csr_name+'_'+k+'-'])
+      else:
+         isa_yaml['hart0'][csr_name][k]=values['-'+csr_name+'_'+k+'-']
+   if sub_field ==[]:
+      for k in list(set(isa_yaml['hart0'][csr_name][rvxlen].keys()) - set(['fields'])):
+        update = values['-'+csr_name+'_'+k+'-']
+        if k in {'msb', 'lsb'}:
+           update = int(update)
+        elif k == 'type':
+           update=eval(update)
+        elif k == 'implemented':
+            if update=='True':
+              update=True
+            else:
+             update=False
+        else:
+           update = update
+        isa_yaml['hart0'][csr_name][rvxlen][k]= update
+   else:
+     for sub in sub_field:
+      for k in list(set(isa_yaml['hart0'][csr_name][rvxlen][sub].keys()) - set(['fields'])):
+          update = values['-'+csr_name+'_'+sub+k+'-']
+          if k in {'msb', 'lsb'}:
+           update = int(update)
+          elif k == 'type':
+           update=eval(update)
+          elif k == 'implemented':
+            if update=='True':
+              update=True
+            else:
+             update=False
+          else:
+           update = update
+          isa_yaml['hart0'][csr_name][rvxlen][sub][k]=update
+   f=open(ispec, 'w')
+   utils.dump_yaml(isa_yaml, f)
+   
+def inner(csr):
+  global csr_name, sub_field
+  csr_name=csr
+  sub_field=list(set(isa_yaml['hart0'][csr][rvxlen].keys()) - set(['fields', 'msb', 'lsb', 'accessible', 'shadow', 'shadow_type','type']))
+  if sub_field == [] :
+     rr= [[sg.Text(k), sg.InputText(isa_yaml['hart0'][csr][rvxlen][k], key='-'+csr+'_'+k+'-') ] for k in list(set(isa_yaml['hart0'][csr][rvxlen].keys()) - set(['fields', 'type'])) ]
+     _type= [[sg.Text('type'), sg.InputText(eval(str(isa_yaml['hart0'][csr][rvxlen]['type']).replace('ordereddict','dict')), key='-'+csr+'_type-') ]]
+  else:
+    rr=[[sg.Column([[sg.Frame(sub, [[sg.Text(k), sg.InputText(isa_yaml['hart0'][csr][rvxlen][sub][k], key='-'+csr+'_'+sub+k+'-') ] for k in list(set(isa_yaml['hart0'][csr][rvxlen][sub].keys()) -set(['fields','type'])) ]+ [[sg.Text('type'), sg.InputText(eval(str(isa_yaml['hart0'][csr][rvxlen][sub]['type']).replace('ordereddict','dict')), key='-'+csr+'_'+sub+'type-') ]]) ]  for sub in sub_field] , pad=(0,0), scrollable=True, key = "Columnmmm", size=(400,500))]]
+    _type= [[]]
+  return [[sg.Text(k), sg.InputText(isa_yaml['hart0'][csr][k], key='-'+csr+'_'+k+'-') ] for k in list(set(isa_yaml['hart0'][csr].keys())-set(['rv32', 'rv64'])) ] + rr +_type
+         
+
+def print_csrs(csr):
+  ''' 
+   To print required number of csrs '''
+  
+  col1 = sg.Column([[sg.Frame(csr, inner(csr))]], pad=(0,0), scrollable=True, key = "Column", size=(450,500))
+  return  col1
+        
+def page1(csr):
+    global values
+    col3 = sg.Column([[sg.Frame('Actions:',
+                            [[sg.Column([[sg.Button('Back To Index'), sg.Button('Save'), ]],
+                                        size=(450,45), pad=(0,0))]])]], pad=(0,0))
+    col1=print_csrs(csr)
+    layout=[[col1],
+            [col3]]
+    window = sg.Window('PAGE1', layout)
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        if event == 'Save':
+           update_fields()
+           window.close()
+           
+def index():
+  global isa_yaml, rvxlen
+  with open(ispec, 'r') as file:
+    isa_yaml = yaml.load(file)
+    rvxlen= 'rv'+str(isa_yaml['hart0']['supported_xlen'][0])
+  csr=[]
+  for field in isa_yaml['hart0']:
+       if isinstance(isa_yaml['hart0'][field], dict) and (isa_yaml['hart0'][field][rvxlen]['accessible']==True):
+          csr.append(str(field))  
+  csr.sort()
+  col2=[[sg.Button(str(csr[(j*10)+(i+1)]), size=(12,0)) for i in range(0,10)] for j in range(0, int(len(csr)/10))]
+  layout=[col2]
+  window1 = sg.Window('LIST OF CSRS', layout)
+  while True:
+      event, values = window1.read()
+      page1(str(event))
+      if event == sg.WIN_CLOSED:
+          break
+         
+            
+def gui_page2(isa_spec, work_dir): 
+    global pagenum
+    global ispec, wdir
+    ispec=isa_spec
+    wdir=work_dir       
+    index()
